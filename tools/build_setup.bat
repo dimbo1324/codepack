431189731634a0f24c30de7cfd94e68d3094b9c5
dimbo1324 <dimaprihodko180@gmail.com>
2026-06-29 08:59:41 +0300

    ...

--- tools/build_setup.bat
+++ tools/build_setup.bat
@@ -11,6 +11,7 @@ set "ISCC_EXE="
 if exist "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" set "ISCC_EXE=%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe"
 if not defined ISCC_EXE if exist "%ProgramFiles%\Inno Setup 6\ISCC.exe" set "ISCC_EXE=%ProgramFiles%\Inno Setup 6\ISCC.exe"
 if not defined ISCC_EXE if exist "%LocalAppData%\Programs\Inno Setup 6\ISCC.exe" set "ISCC_EXE=%LocalAppData%\Programs\Inno Setup 6\ISCC.exe"
+if not defined ISCC_EXE if exist "C:\scripts\Inno Setup 6\utils\Inno\ISCC.exe" set "ISCC_EXE=C:\scripts\Inno Setup 6\utils\Inno\ISCC.exe"
 if defined ISCC_EXE (
   "%ISCC_EXE%" "build\installer\ProjectExporterDesktop.iss"
   exit /b %ERRORLEVEL%
